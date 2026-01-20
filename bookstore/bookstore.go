eed7834851c7b6ff6a1561b43adace63ddf4403c
itsfarhan <itsmefarhan09@gmail.com>
2026-01-20 23:45:46 +0530

    updated bookstore story
    Signed-off-by: itsfarhan <itsmefarhan09@gmail.com>

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,27 +1,33 @@
 package bookstore
 
-import (
-	"errors"
-	// "fmt"
-	
-)
+//"errors"
+// "fmt"
 
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+	ISBN   int
 }
 
-var books = []Book {
-	{Title: "Harry Potter"},
-	{Title: "SuperGirl: Woman of Tommorrow"},
-}
-
-func Buy(b Book) (Book, error) {
-	if b.Copies == 0 {
-		return Book{}, errors.New("no copies available")
+func GetAllBooks(catalog []Book, ISBN int) Book {
+	for _, b := range catalog {
+		if b.ISBN == ISBN {
+			return b
+		}
 	}
-	b.Copies--
-	return b, nil
+	return Book{} // why Book{} -
 }
 
+// var books = []Book {
+// 	{Title: "Harry Potter"},
+// 	{Title: "SuperGirl: Woman of Tommorrow"},
+// }
+
+// func Buy(b Book) (Book, error) {
+// 	if b.Copies == 0 {
+// 		return Book{}, errors.New("no copies available")
+// 	}
+// 	b.Copies--
+// 	return b, nil
+// }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -3,65 +3,63 @@ package bookstore_test
 import (
 	"bookstore"
 	"testing"
-
 	"github.com/google/go-cmp/cmp"
 )
-
-func TestBookstore(t *testing.T) {
-	t.Parallel()
-	// Placeholder for future tests related to the bookstore package
-	_ = bookstore.Book{
-		Title:  "Farhan Go-Practice",
-		Author: "Farhan",
-		Copies: 10,
-	}
-}
-
-func TestBuy(t *testing.T) {
-	t.Parallel()
-	// Placeholder
-	b := bookstore.Book{
-		Title:  "Farhan Go-Practice",
-		Author: "Farhan",
-		Copies: 2,
-	}
-
-	want := 1
-	result, err := bookstore.Buy(b)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-	got := result.Copies
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
-	}
-}
-
-func TestBuyErrorIfNoCopies(t *testing.T) {
-	t.Parallel()
-	var b = bookstore.Book{
-		Title:  "Farhan Go-Practice",
-		Author: "Farhan",
-		Copies: 0,
-	}
-	_, err := bookstore.Buy(b)
-	if err == nil {
-		t.Fatalf("want error buying from 0 copies, got nil")
-	}
-}
 
 func TestGetAllBooks(t *testing.T) {
 	t.Parallel()
 	catalog := []bookstore.Book{
-		{Title: "For the Love of Go"},
-		{Title: "The Power of Go: Tools"},
+		{Title: "Harry potter", ISBN: 1},
+		// {Title: "Superman", ISBN: 2},	
 	}
-	want := []bookstore.Book{
-		{Title: "For the Love of Go"},
-		{Title: "The Power of Go: Tools"},
-	}
-	got := bookstore.GetAllBooks(catalog)
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
+	want := bookstore.Book{Title: "Harry potter", ISBN: 1}
+		// {Title: "Superman", ISBN: 2},
+
+	got := bookstore.GetAllBooks(catalog, 1) // here bookstore.GetAllBooks called catalog for getting all books
+	if !cmp.Equal(want, got) { // here cmp is used to compare want and got 
+		t.Error(cmp.Diff(want, got)) // Equal and diff are methods of cmp package and used to compare two values
 	}
 }
+
+// func TestBookstore(t *testing.T) {
+// 	t.Parallel()
+// 	// Placeholder for future tests related to the bookstore package
+// 	_ = bookstore.Book{
+// 		Title:  "Farhan Go-Practice",
+// 		Author: "Farhan",
+// 		Copies: 10,
+// 	}
+// }
+
+// func TestBuy(t *testing.T) {
+// 	t.Parallel()
+// 	// Placeholder
+// 	b := bookstore.Book{
+// 		Title:  "Farhan Go-Practice",
+// 		Author: "Farhan",
+// 		Copies: 2,
+// 	}
+
+// 	want := 1
+// 	result, err := bookstore.Buy(b)
+// 	if err != nil {
+// 		t.Fatalf("%v", err)
+// 	}
+// 	got := result.Copies
+// 	if want != got {
+// 		t.Errorf("want %d, got %d", want, got)
+// 	}
+// }
+
+// func TestBuyErrorIfNoCopies(t *testing.T) {
+// 	t.Parallel()
+// 	var b = bookstore.Book{
+// 		Title:  "Farhan Go-Practice",
+// 		Author: "Farhan",
+// 		Copies: 0,
+// 	}
+// 	_, err := bookstore.Buy(b)
+// 	if err == nil {
+// 		t.Fatalf("want error buying from 0 copies, got nil")
+// 	}
+// }
