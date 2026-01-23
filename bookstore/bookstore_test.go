52a07b3d821f087c1306b200332d5278a0ed6d99
itsfarhan <itsmefarhan09@gmail.com>
2026-01-23 23:47:02 +0530

    maps practice
    Signed-off-by: itsfarhan <itsmefarhan09@gmail.com>

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -2,19 +2,26 @@ package bookstore_test
 
 import (
 	"bookstore"
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestGetBook(t *testing.T) {
+func TestGetAllBooks(t *testing.T) {
 	t.Parallel()
 	catalog := map[int]bookstore.Book{
 		1: {ID: 1, Title: "For the Love of Go"},
 		2: {ID: 2, Title: "The Power of Go: Tools"},
 	}
-	want := bookstore.Book{ID: 2, Title: "The Power of Go: Tools"}
-	got := bookstore.GetBook(catalog, 2)
+	want := []bookstore.Book{
+		{ID: 1, Title: "For the Love of Go"},
+		{ID: 2, Title: "The Power of Go: Tools"},
+	}
+	got := bookstore.GetBook(catalog)
+	sort.Slice(got, func(i, j int) bool {
+		return got[i].ID < got[j].ID
+	})
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
 	}
