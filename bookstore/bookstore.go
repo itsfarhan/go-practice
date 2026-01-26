109195be07ae54a9c0c0e6037605da459a205492
itsfarhan <itsmefarhan09@gmail.com>
2026-01-27 00:29:49 +0530

    maps
    Signed-off-by: itsfarhan <itsmefarhan09@gmail.com>

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,20 +1,30 @@
 package bookstore
 
 import (
-	"errors"
-	// "fmt"
+	// "errors"
+	"fmt"
 )
 
 type Book struct {
 	Title  string
 	Author string
 	Copies int
-	ID   int
+	ID     int
 }
 
 func GetBook(catalog map[int]Book, ID int) (Book, error) {
-	return catalog[ID], nil
+	b, ok := catalog[ID] // Here, ok is boolean (true or false)
+	if !ok {             //
+		return Book{}, fmt.Errorf("ID %d doesn't exist", ID)
+	}
+	return b, nil
 }
+
+// modify maps in Go is done by first retrieving the struct, modifying its fields, and then reassigning it back to the map.
+
+// b := catalog[1]
+// b.Title = "New Title"
+// catalog[1] = b
 
 // var books = []Book {
 // 	{Title: "Harry Potter"},
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -2,28 +2,39 @@ package bookstore_test
 
 import (
 	"bookstore"
-	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestGetAllBooks(t *testing.T) {
+func TestGetBook(t *testing.T) {
 	t.Parallel()
+	// Here, catalog is a map with book ID as key and Book struct as value
 	catalog := map[int]bookstore.Book{
-		1: {ID: 1, Title: "For the Love of Go"},
-		2: {ID: 2, Title: "The Power of Go: Tools"},
+		1: {ID: 1, Title: "Harry Potter"},
+		2: {ID: 2, Title: "SuperWoman"},
 	}
-	want := []bookstore.Book{
-		{ID: 1, Title: "For the Love of Go"},
-		{ID: 2, Title: "The Power of Go: Tools"},
+	want := bookstore.Book{ID: 2, Title: "SuperWoman"}
+
+	// Here, we are testing the GetBook function to retrieve the book with ID 2
+	got, err := bookstore.GetBook(catalog, 2) // got is a single Book struct and its a result of GetBook function. err is the error returned by GetBook function.
+	if err != nil {                           // if err is not nil, then there was an error in getting the book
+		// Here, we use t.Fatal to log the error and stop the test immediately
+		t.Fatal(err) // if is use fatalf then I need to use format specifier like - t.Fatalf("error: %v", err)
 	}
-	got := bookstore.GetBook(catalog, 2)
-	sort.Slice(got, func(i, j int) bool {
-		return got[i].ID < got[j].ID
-	})
+	// Here, we compare the want and got values using cmp.Equal
 	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
+		t.Error(cmp.Diff(want, got)) // Here, we log the difference between want and got using cmp.Diff. It will show what is missing in got compared to want using '-' and what is extra in got compared to want using '+'
+	}
+}
+
+// Here, we are adding a test case to check for non-existing book ID
+func TestGetBookBadIDreturnsError(t *testing.T) {
+	t.Parallel()
+	catalog := map[int]bookstore.Book{}
+	_, err := bookstore.GetBook(catalog, 999)
+	if err == nil {
+		t.Fatal("want error for non-exist ID, but I got nil")
 	}
 }
 
@@ -66,6 +77,6 @@ func TestGetAllBooks(t *testing.T) {
 // 	}
 // 	_, err := bookstore.Buy(b)
 // 	if err == nil {
-// 		t.Fatalf("want error buying from 0 copies, got nil")
+// 		t.Fatalf("want error because cant but 0 copies, but got nil")
 // 	}
 // }
